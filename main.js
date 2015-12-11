8e12d0ad5fe3dd0f366aa3bd8b75dd766d007147
Angelica Barreda B <anjelyk.b@gmail.com>
2015-12-11 16:02:17 -0600

    Integración de pantalla LCD
    Integración de código  para pantalla LCD

--- main.js
+++ main.js
@@ -21,6 +21,9 @@ var myVolts3 = new IRProximity.GP2Y0A(3);
 var myVolts4 = new IRProximity.GP2Y0A(4);
 var myVolts5 = new IRProximity.GP2Y0A(5);
 
+var useUpmVersion = true;
+var mraa = require('mraa');
+
 // The higher the voltage (closer to AREF) the closer the object is.
 // NOTE: The measured voltage will probably not exceed 3.3 volts.
 // Every second, print the averaged voltage value
@@ -42,3 +45,92 @@ process.on('SIGINT', function()
 	console.log("Exiting...");
 	process.exit(0);
 });
+
+
+
+
+var version = mraa.getVersion();
+
+if (version >= 'v0.6.1') {
+    console.log('mraa version (' + version + ') ok');
+}
+else {
+    console.log('meaa version(' + version + ') is old - this code may not work');
+}
+
+if (useUpmVersion) {
+    useUpm();
+}
+else {
+    useLcd();
+}
+
+/**
+ * Rotate through a color pallette and display the
+ * color of the background as text
+ */
+function rotateColors(display) {
+    var red = 0;
+    var green = 0;
+    var blue = 0;
+    display.setColor(red, green, blue);
+    setInterval(function() {
+        blue += 64;
+        if (blue > 255) {
+            blue = 0;
+            green += 64;
+            if (green > 255) {
+                green = 0;
+                red += 64;
+                if (red > 255) {
+                    red = 0;
+                }
+            }
+        }
+        display.setColor(red, green, blue);
+        display.setCursor(0,0);
+        display.write('red=' + red + ' grn=' + green + '  ');
+        display.setCursor(1,0);
+        display.write('blue=' + blue + '   ');  // extra padding clears out previous text
+    }, 1000);
+}
+
+/**
+ * Use the upm library to drive the two line display
+ *
+ * Note that this does not use the "lcd.js" code at all
+ */
+function useUpm() {
+    var lcd = require('jsupm_i2clcd');
+    var display = new lcd.Jhd1313m1(0, 0x3E, 0x62);
+    display.setCursor(1, 1);
+    display.write('Angie');
+    display.setCursor(0,0);
+    display.write('angieee');
+    rotateColors(display);
+}
+
+/**
+ * Use the hand rolled lcd.js code to do the
+ * same thing as the previous code without the
+ * upm library
+ */
+function useLcd() {
+    var lcd = require('./lcd');
+    var display = new lcd.LCD(0);
+
+    display.setColor(0, 60, 255);
+    display.setCursor(1, 1);
+    display.write('hi there');
+    display.setCursor(0,0);  
+    display.write('more text');
+    display.waitForQuiescent()
+    .then(function() {
+        rotateColors(display);
+    })
+    .fail(function(err) {
+        console.log(err);
+        display.clearError();
+        rotateColors(display);
+    });
+}
